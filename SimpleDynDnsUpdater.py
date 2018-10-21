f6aa0581362806ebd39b77544006c1fbdfc2fef3
Lucas Zanela <me@lucaszanella.com>
2018-10-21 10:03:42 -0300

    Update SimpleDynDnsUpdater.py

--- SimpleDynDnsUpdater.py
+++ SimpleDynDnsUpdater.py
@@ -3,22 +3,22 @@ import time
 
 username = 'username'
 password = 'password'
-hostname = 'hostname.example.com'
+hostname = 'domain.example.com'
 time_to_repeat = 60*3 #3 minutes
-#ipv4 = '123.456.789.012'
-#ipv6 = '2001:1ab8:2102:f4c1:ca0e:145f:2ee9:8366'
+enable_ipv6 = False #On docker you might not have ipv6 and thus get an error instead of updating things. Can somebody correct that for me?
 
 while True:
-	try:
-		ipv4 = requests.get('http://v4.ipv6-test.com/api/myip.php').text
-		print(ipv4)
-		ipv6 = requests.get('http://v6.ipv6-test.com/api/myip.php').text
-		print(ipv6)
-		#ipv4v6 = requests.get('http://v4v6.ipv6-test.com/api/myip.php').text
-		#print(ipv4v6)
-
-		r = requests.get('https://members.dyndns.org/v3/update?hostname=' + hostname + '&myip=' + ipv4 + ',' + ipv6, auth=(username, password))
-		print(r.text)
-	except Exception as e:
-		print('error: ' + e)
-	time.sleep(time_to_repeat)
+        try:
+                ipv4 = requests.get('http://v4.ipv6-test.com/api/myip.php').text
+                print(ipv4)
+                if not enable_ipv6:
+                        r = requests.get('https://members.dyndns.org/v3/update?hostname=' + hostname + '&myip=' + ipv4, auth=(username, password))
+                        print(r.text)
+                else:
+                        ipv6 = requests.get('http://v6.ipv6-test.com/api/myip.php').text
+                        print(ipv6)
+                        r = requests.get('https://members.dyndns.org/v3/update?hostname=' + hostname + '&myip=' + ipv4 + ',' + ipv6, auth=(username, password))
+                        print(r.text)
+        except Exception as e:
+                print('error: ' + str(e))
+        time.sleep(time_to_repeat)
